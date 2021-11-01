ce9b2df368178481dc08388be27525bc4319fa49
Prof. Doo <59063788+paisleypark3121@users.noreply.github.com>
2021-11-01 14:39:20 +0000

    SJF fix

--- sjf.js
+++ sjf.js
@@ -87,6 +87,8 @@ function sjf()
   }
 
   processes.sort(function(a,b) {
+    if (a[1]>b[1]) return 1;
+    if (a[1]<b[1]) return -1;
     if (a[0]>b[0]) return 1;
     if (a[0]<b[0]) return -1;
     if (a[0]==b[0]) return 0;
